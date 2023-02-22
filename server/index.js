fca71deb637a9da78a9a1b287a8bda9dd18336f9
tommy solano <tommysolano18@hotmail.com>
2023-02-22 00:26:51 -0500

    base setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.listen(4000,()=> {
+    console.log("server started on port 4000")
+})
+
+mongoose.connect("mongodb://localhost:27017/luba",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> {
+    console.log("db connection successfull")
+}).catch(err=>{
+    console.log(err.message)
+})
+
+app.use(cors({
+    origin: ["https://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true,
+}))
+
+app.use(express.json())
